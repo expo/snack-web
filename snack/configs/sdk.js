f2c7004a07f1eb863713b449a31c81aa3da18040
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2018-08-31 01:26:31 +0000

    Drop SDK 23 (#3060)
    fbshipit-source-id: f9c7048

--- snack/configs/sdk.js
+++ snack/configs/sdk.js
@@ -1,7 +1,6 @@
 /* @flow */
 
 export const versions = {
-  '23.0.0': true,
   '24.0.0': true,
   '25.0.0': true,
   '26.0.0': true,
@@ -11,6 +10,6 @@ export const versions = {
 };
 
 export const DEFAULT_SDK_VERSION = '29.0.0';
-export const FALLBACK_SDK_VERSION = '23.0.0';
+export const FALLBACK_SDK_VERSION = '24.0.0';
 
 export type SDKVersion = $Keys<typeof versions>;
