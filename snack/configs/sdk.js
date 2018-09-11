0e24489f2fb2bd1a3e24f52d09748b61740cd488
TC Davis <thomas.c.davis@gmail.com>
2018-09-11 21:54:07 +0000

    Upgrade snack-web for SDK 30
    fbshipit-source-id: b4be6a0

--- snack/configs/sdk.js
+++ snack/configs/sdk.js
@@ -7,9 +7,10 @@ export const versions = {
   '27.0.0': true,
   '28.0.0': true,
   '29.0.0': true,
+  '30.0.0': true,
 };
 
-export const DEFAULT_SDK_VERSION = '29.0.0';
+export const DEFAULT_SDK_VERSION = '30.0.0';
 export const FALLBACK_SDK_VERSION = '24.0.0';
 
 export type SDKVersion = $Keys<typeof versions>;
