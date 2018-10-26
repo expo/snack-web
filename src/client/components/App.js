c93524864b0b215cb8593cd82f8a2cb285d1aadb
TC Davis <thomas.c.davis@gmail.com>
2018-10-26 17:10:12 +0000

    Add more tutorial content to new project readme
    fbshipit-source-id: 566b7b3

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -133,9 +133,15 @@ const styles = StyleSheet.create({
   'README.md': {
     contents: `# Sample Snack app
 
-Open the \`App.js\` file to start writing some code. You preview the changes directly on your device or simulator by clicking the **Run** button. When you're done, click **Save** and share the link!
+Welcome to Expo!
 
-If you're having problems, you can tweet to us [@expo](https://twitter.com/expo).
+Open the \`App.js\` file to start writing some code. You can preview the changes directly on your phone or tablet by clicking the **Run** button or use the simulator by clicking **Tap to Play**. When you're done, click **Save** and share the link!
+
+When you're ready to see everything that Expo provides (or if you want to use your own editor) you can **Export** your project and use it with [expo-cli](https://docs.expo.io/versions/latest/introduction/installation.html).
+
+Projects created in Snack are publicly available, so you can easily share the link to this project via link, or embed it on a web page with the **Embed** button.
+
+If you're having problems, you can tweet to us [@expo](https://twitter.com/expo) or ask in our [forums](https://forums.expo.io).
 
 Snack is Open Source. You can find the code on the [GitHub repo](https://github.com/expo/snack-web).
 `,
@@ -855,7 +861,8 @@ class App extends React.Component<Props, State> {
               <EmbeddedShell />
             ) : (
               <AppShell title={title} />
-            )}
+            )
+          }
         </LazyLoad>
       );
     }
