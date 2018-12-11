be9cfe667151b1fca309ee212598f79f287bacc0
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-12-11 19:40:26 +0000

    Fix syntax highlighting typescript in simple editor
    fbshipit-source-id: 9db1c8f

--- src/client/components/Editor/SimpleEditor.tsx
+++ src/client/components/Editor/SimpleEditor.tsx
@@ -7,6 +7,7 @@ import escape from 'escape-html';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-json';
@@ -66,7 +67,9 @@ class SimpleEditor extends React.Component<Props> {
   }
 
   _highlight = (code: string) => {
-    if (this.props.path.endsWith('.js')) {
+    if (this.props.path.endsWith('.ts') || this.props.path.endsWith('.tsx')) {
+      return highlight(code, languages.ts);
+    } else if (this.props.path.endsWith('.js')) {
       return highlight(code, languages.jsx);
     } else if (this.props.path.endsWith('.json')) {
       return highlight(code, languages.json);
--- src/client/components/Markdown/MarkdownPreview.tsx
+++ src/client/components/Markdown/MarkdownPreview.tsx
@@ -8,6 +8,7 @@ import sanitize from 'sanitize-html';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-json';
