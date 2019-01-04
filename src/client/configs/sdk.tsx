bd5387ebe838052ea56c9eef1d01afe18a0cec46
TC Davis <thomas.c.davis@gmail.com>
2019-01-04 23:02:50 +0000

    Upgrade client to SDK 32
    fbshipit-source-id: 41c192f

--- src/client/configs/sdk.tsx
+++ src/client/configs/sdk.tsx
@@ -5,9 +5,10 @@ export const versions = {
   '29.0.0': true,
   '30.0.0': true,
   '31.0.0': true,
+  '32.0.0': true,
 };
 
-export const DEFAULT_SDK_VERSION = '31.0.0';
+export const DEFAULT_SDK_VERSION = '32.0.0';
 export const FALLBACK_SDK_VERSION = '26.0.0';
 
 export type SDKVersion = keyof typeof versions;
