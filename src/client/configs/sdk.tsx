ef5e6c2014dddea59d2f15554567f491a51753b0
TC Davis <thomas.c.davis@gmail.com>
2019-05-31 22:12:58 +0000

    Drop SDK versions before 31
    fbshipit-source-id: 1aa9762

--- src/client/configs/sdk.tsx
+++ src/client/configs/sdk.tsx
@@ -1,14 +1,9 @@
 export const versions = {
-  '26.0.0': true,
-  '27.0.0': true,
-  '28.0.0': true,
-  '29.0.0': true,
-  '30.0.0': true,
   '31.0.0': true,
   '32.0.0': true,
 };
 
 export const DEFAULT_SDK_VERSION = '32.0.0';
-export const FALLBACK_SDK_VERSION = '26.0.0';
+export const FALLBACK_SDK_VERSION = '32.0.0';
 
 export type SDKVersion = keyof typeof versions;
