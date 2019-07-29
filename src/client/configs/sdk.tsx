415f16b714f70087ebd44bc1436af1010a069b3d
TC Davis <thomas.c.davis@gmail.com>
2019-07-29 23:49:04 +0000

    Snack -> SDK 34
    fbshipit-source-id: 86d1b7b

--- src/client/configs/sdk.tsx
+++ src/client/configs/sdk.tsx
@@ -2,9 +2,10 @@ export const versions = {
   '31.0.0': true,
   '32.0.0': true,
   '33.0.0': true,
+  '34.0.0': true,
 };
 
-export const DEFAULT_SDK_VERSION = '33.0.0';
+export const DEFAULT_SDK_VERSION = '34.0.0';
 export const FALLBACK_SDK_VERSION = '32.0.0';
 export const TEST_SDK_VERSION = '32.0.0'
 
