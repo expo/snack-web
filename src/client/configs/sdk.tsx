eb55b7c6b13a1926b2e4160d9949ea67adda975a
TC Davis <thomas.c.davis@gmail.com>
2019-09-19 22:38:03 +0000

    Drop support for SDKs 31 & 32
    fbshipit-source-id: b74fa47

--- src/client/configs/sdk.tsx
+++ src/client/configs/sdk.tsx
@@ -1,12 +1,10 @@
 export const versions = {
-  '31.0.0': true,
-  '32.0.0': true,
   '33.0.0': true,
   '34.0.0': true,
 };
 
 export const DEFAULT_SDK_VERSION = '34.0.0';
-export const FALLBACK_SDK_VERSION = '32.0.0';
-export const TEST_SDK_VERSION = '32.0.0'
+export const FALLBACK_SDK_VERSION = '34.0.0';
+export const TEST_SDK_VERSION = '34.0.0'
 
 export type SDKVersion = keyof typeof versions;
