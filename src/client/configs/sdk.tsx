bbeffd229503f61ff35527a7afdd5538bacdabd6
TC Davis <thomas.c.davis@gmail.com>
2019-09-20 01:12:42 +0000

    Upgrade snack for SDK 35
    fbshipit-source-id: 8ddbd73

--- src/client/configs/sdk.tsx
+++ src/client/configs/sdk.tsx
@@ -1,10 +1,11 @@
 export const versions = {
   '33.0.0': true,
   '34.0.0': true,
+  '35.0.0': true,
 };
 
-export const DEFAULT_SDK_VERSION = '34.0.0';
+export const DEFAULT_SDK_VERSION = '35.0.0';
 export const FALLBACK_SDK_VERSION = '34.0.0';
-export const TEST_SDK_VERSION = '34.0.0'
+export const TEST_SDK_VERSION = '34.0.0';
 
 export type SDKVersion = keyof typeof versions;
