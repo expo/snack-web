21c710d63f85f2663b230e1d3099af27faef1d6a
Brent Vatne <brentvatne@gmail.com>
2019-11-26 22:22:00 +0000

    Pass through search params in getSnackURLFromEmbed
    fbshipit-source-id: 204a202

--- src/client/utils/getSnackURLFromEmbed.tsx
+++ src/client/utils/getSnackURLFromEmbed.tsx
@@ -3,8 +3,10 @@ import compact from 'lodash/compact';
 export default function getSnackURLFromEmbed(sessionID: string) {
   const protocol = window.location.protocol;
   const host = window.location.host;
+  const search = window.location.search ? window.location.search.replace(/^\?/, '&') : '';
+
   const pathname = compact(window.location.pathname.split('/'));
   pathname.shift();
 
-  return `${protocol}//${host}/${pathname.join('/')}?session_id=${sessionID}`;
+  return `${protocol}//${host}/${pathname.join('/')}?session_id=${sessionID}${search}`;
 }
