341a1aee501b3ce646fe21677ff0f6565b05fc7f
TC Davis <thomas.c.davis@gmail.com>
2019-11-28 00:05:44 +0000

    Build please, thank you CI
    fbshipit-source-id: e584050

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -139,7 +139,7 @@ Open the \`App.js\` file to start writing some code. You can preview the changes
 
 When you're ready to see everything that Expo provides (or if you want to use your own editor) you can **Export** your project and use it with [expo-cli](https://docs.expo.io/versions/latest/introduction/installation.html).
 
-Projects created in Snack are publicly available, so you can easily share the link to this project via link, or embed it on a web page with the **Embed** button.
+All projects created in Snack are publicly available, so you can easily share the link to this project via link, or embed it on a web page with the **Embed** button.
 
 If you're having problems, you can tweet to us [@expo](https://twitter.com/expo) or ask in our [forums](https://forums.expo.io).
 
