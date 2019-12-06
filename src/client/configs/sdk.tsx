d9528002a2a9698ea1baf5714ffa6eae2c7d6565
TC Davis <thomas.c.davis@gmail.com>
2019-12-06 01:12:14 +0000

    Update snack client for SDK 36
    fbshipit-source-id: b37558d

--- src/client/configs/sdk.tsx
+++ src/client/configs/sdk.tsx
@@ -2,9 +2,10 @@ export const versions = {
   '33.0.0': true,
   '34.0.0': true,
   '35.0.0': true,
+  '36.0.0': true,
 };
 
-export const DEFAULT_SDK_VERSION = '35.0.0';
+export const DEFAULT_SDK_VERSION = '36.0.0';
 export const FALLBACK_SDK_VERSION = '34.0.0';
 export const TEST_SDK_VERSION = '34.0.0';
 
